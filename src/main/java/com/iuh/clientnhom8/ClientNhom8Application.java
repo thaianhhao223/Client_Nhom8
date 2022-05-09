0e8c7a3a48116ed8a88aa8192aefd5c6e0f7bfe7
henry.thai <henry.thai@alpaca.vn>
2022-05-09 21:30:39 +0700

    base commit

---
+++ src/main/java/com/iuh/clientnhom8/ClientNhom8Application.java
@@ -0,0 +1,13 @@
+package com.iuh.clientnhom8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientNhom8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientNhom8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/iuh/clientnhom8/ClientNhom8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.clientnhom8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientNhom8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
