9b73fa0c95f9db76c9d97125dc795fa6173fcd43
eric <eric.tran@alpaca.vn>
2022-05-11 22:48:57 +0700

    Add many ui for admin

---
+++ src/main/java/com/iuh/clientnhom8/controller/AdminTestController.java
@@ -0,0 +1,51 @@
+package com.iuh.clientnhom8.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("admin")
+public class AdminTestController {
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String hello(){
+        return "single-product";
+    }
+    @RequestMapping(value = "/cart",method = RequestMethod.GET)
+    public String cart(){
+        return "cart";
+    }
+    @RequestMapping(value = "/checkout",method = RequestMethod.GET)
+    public String checkout(){
+        return "checkout";
+    }
+    @RequestMapping(value = "/category",method = RequestMethod.GET)
+    public String category(){
+        return "category";
+    }
+    @RequestMapping(value = "/confirmation",method = RequestMethod.GET)
+    public String confirmation(){
+        return "confirmation";
+    }
+    @RequestMapping(value = "/contact",method = RequestMethod.GET)
+    public String contact(){
+        return "contact";
+    }
+    @RequestMapping(value = "/elements",method = RequestMethod.GET)
+    public String elements(){
+        return "elements";
+    }
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login(){
+        return "login";
+    }
+    @RequestMapping(value = "/registration",method = RequestMethod.GET)
+    public String registration(){
+        return "registration";
+    }
+    @RequestMapping(value = "/tracking",method = RequestMethod.GET)
+    public String tracking(){
+        return "tracking";
+    }
+}
+
---
+++ src/main/java/com/iuh/clientnhom8/controller/TestUiAdminController.java
@@ -0,0 +1,57 @@
+package com.iuh.clientnhom8.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/admin")
+public class TestUiAdminController {
+    @GetMapping("/home")
+    public String homeAdmin(){
+        return "dashboard-admin";
+    }
+    @GetMapping("/account")
+    public String accountAdmin(){
+        return "account-admin";
+    }
+    @GetMapping("/customer")
+    public String customerAdmin(){
+        return "customer-admin";
+    }
+    @GetMapping("/type")
+    public String typeAdmin(){
+        return "type-admin";
+    }
+    @GetMapping("/brand")
+    public String brandAdmin(){
+        return "brand-admin";
+    }
+    @GetMapping("/product")
+    public String productAdmin(){
+        return "product-admin";
+    }
+    @GetMapping("/billing")
+    public String billAdmin(){
+        return "billing-admin";
+    }
+    @GetMapping("/customer/customer-order")
+    public String customerOrderAdmin(){
+        return "customer-order-admin";
+    }
+    @GetMapping("/profile")
+    public String profileAdmin(){
+        return "profile-admin";
+    }
+    @GetMapping("/signin")
+    public String signinAdmin(){
+        return "sign-in-admin";
+    }
+    @GetMapping("/signup")
+    public String signupAdmin(){
+        return "sign-up-admin";
+    }
+}
